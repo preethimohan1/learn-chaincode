4e5a9b932709f5b6637cef5d2de9cb03c305b964
Preethi Mohan <preethi.mohan@in.ibm.com>
2017-08-01 12:25:38 +0530

    Remove passing stub in function call

--- energyTrading/eTradingChainCode.go
+++ energyTrading/eTradingChainCode.go
@@ -65,19 +65,19 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	
 	//create Maps for Each Type of User
 	producerInfoMap := make(map[string]*[]byte)
-    	addTestUser(shim.ChaincodeStubInterface, producerInfoMap, "producer", "Producer", "Producer Company 1", "Producer Company Location", "producer", 3456, 10000.0)
+    	addTestUser(producerInfoMap, "producer", "Producer", "Producer Company 1", "Producer Company Location", "producer", 3456, 10000.0)
 	producerInfoMapBytes, _ := json.Marshal(producerInfoMap)
     
 	shipperInfoMap := make(map[string]*[]byte)
-    	addTestUser(shim.ChaincodeStubInterface, shipperInfoMap, "shipper", "Shipper", "Shipper Company 1", "Shipper Company Location", "shipper", 1234, 10000.0)
+    	addTestUser(shipperInfoMap, "shipper", "Shipper", "Shipper Company 1", "Shipper Company Location", "shipper", 1234, 10000.0)
 	shipperInfoMapBytes, _ := json.Marshal(shipperInfoMap)
     
 	buyerInfoMap := make(map[string]*[]byte)
-    	addTestUser(shim.ChaincodeStubInterface, buyerInfoMap, "buyer", "Buyer", "Buyer Company 1", "Buyer Company Location", "buyer", 4567, 10000.0)
+    	addTestUser(buyerInfoMap, "buyer", "Buyer", "Buyer Company 1", "Buyer Company Location", "buyer", 4567, 10000.0)
 	buyerInfoMapBytes, _ := json.Marshal(buyerInfoMap)
     
 	transporterInfoMap := make(map[string]*[]byte)
-    	addTestUser(shim.ChaincodeStubInterface, transporterInfoMap, "transporter", "Transporter", "Transporter Company 1", "Transporter Company Location", "transporter", 6789, 10000.0)
+    	addTestUser(transporterInfoMap, "transporter", "Transporter", "Transporter Company 1", "Transporter Company Location", "transporter", 6789, 10000.0)
 	transporterInfoMapBytes, _ := json.Marshal(transporterInfoMap)
 
 	_ = stub.PutState("producerInfoMap", producerInfoMapBytes)
